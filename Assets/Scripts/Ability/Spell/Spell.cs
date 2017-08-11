6ba4f4563634435759c2af841d92f0650d767118
wmysterio <wmysterio@ukr.net>
2017-08-11 20:41:14 +0300

    Додано інтерфейс IAbility, класи Effect, Item, Spell

---
+++ Assets/Scripts/Ability/Spell/Spell.cs
@@ -0,0 +1,48 @@
+﻿/*
+	Ліцензія: CC-BY
+	Автор: Василь ( wmysterio )
+	Сайт: http://www.unity3d.tk/
+*/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MyRPG {
+
+	public abstract class Spell : IAbility {
+
+        private Texture2D icon;
+
+        public TypeOfAbility Type {
+            get { return TypeOfAbility.Spell; }
+        }
+
+        protected string description;
+        protected string name;
+
+        public float TimeToRestore { get; protected set; }
+        public int Count { get; protected set; }
+
+        public Texture2D Icon {
+            get { return icon; }
+            set { icon = value; }
+        }
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Description {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public Spell() { }
+
+        public void Use( Personage traget = null ) { }
+
+    }
+
+}
---
+++ Assets/Scripts/Interfaces/IAbility.cs
@@ -0,0 +1,26 @@
+﻿/*
+	Ліцензія: CC-BY
+	Автор: Василь ( wmysterio )
+	Сайт: http://www.unity3d.tk/
+*/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MyRPG {
+
+    public enum TypeOfAbility { Effect, Item, Spell }
+
+    public interface IAbility : IDescription {
+        
+        TypeOfAbility Type { get; }
+        float TimeToRestore { get; }
+        int Count { get; }
+        void Use( Personage traget = null );
+
+	}
+
+}
